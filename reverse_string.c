0c60d62db2f2e47427d75d95e050e80d9c89f9be
Lohithvsvs <140479040+Lohithvsvs@users.noreply.github.com>
2023-08-07 12:19:08 +0530

    Create reverse_string.c

---
+++ reverse_string.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverseString(char str[]) {
+    int length = strlen(str);
+    for (int i = 0; i < length / 2; i++) {
+        char temp = str[i];
+        str[i] = str[length - i - 1];
+        str[length - i - 1] = temp;
+    }
+}
+
+int main() {
+    char input[100];
+
+    printf("Enter a string: ");
+    fgets(input, sizeof(input), stdin);
+
+    // Remove the newline character from fgets
+    int len = strlen(input);
+    if (len > 0 && input[len - 1] == '\n') {
+        input[len - 1] = '\0';
+    }
+
+    reverseString(input);
+
+    printf("Reversed string: %s\n", input);
+
+    return 0;
+}
